5ae2521413f241b786d7c56b92c2926727dbf075
Justin Lund <47057520+Justin-Lund@users.noreply.github.com>
2020-01-01 17:00:15 -0800

    Delete PS Remote Tools.bat

--- PS Remote Tools.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-:: Launch PowerShell Remote Tools
-:: https://github.com/Justin-Lund/
-
-start powershell -Command Invoke-Command -ScriptBlock {Set-ExecutionPolicy Bypass; C:\Remote-Tools.ps1}
